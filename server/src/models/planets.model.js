2b58f255bbc034322bab2dd7219fc0636f47a292
Christian <bournecreative@gmail.com>
2024-06-19 23:46:25 -0500

    bring in kepler data set downloaded from nasa site, implement logic to read stream from csv and install csv-parse package

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,35 @@
-const planets = ['earth', 'pluto']
+const { parse } = require('csv-parse')
+const fs = require('fs')
 
-module.exports = planets
+const results = []
+
+function isHospitable (planetData) {
+    return planetData['koi_disposition'] === 'CONFIRMED'
+    && planetData['koi_insol'] > 0.36
+    && planetData['koi_insol'] < 1.11
+    && planetData['koi_prad'] < 1.6
+}
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if (isHospitable(data)) {
+            results.push(data)
+        }
+    })
+    .on('error', (err) => {
+        console.log(`Error  ${err}`)
+    })
+    .on('end', () => {
+        console.log(results)
+        console.log('done', results.length)
+    })
+
+module.exports = {
+    planets: results
+}
+
+
